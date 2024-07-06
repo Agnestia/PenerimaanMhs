5d158380cef9be36e67e0f3fb0b6d20c903992bd
djoenaidi26 <agnestiaagustine260804@gmail.com>
2024-07-06 14:36:24 +0700

    agnes, simpan database dan buat database

--- Project_akhir/src/koneksi/KoneksiDatabase.java
+++ Project_akhir/src/koneksi/KoneksiDatabase.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package koneksi;
 
-/**
- *
- * @author agnes
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 public class KoneksiDatabase {
-    
+    public static Connection BukaKoneksi(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); 
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/projek_uas", "root", "");
+            return cn;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
 }
---
+++ Project_akhir/src/ui/MainApp.java
@@ -0,0 +1,16 @@
+
+package ui;
+
+public class MainApp {
+        public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        // ...
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Formulir_MHS().setVisible(true);
+            }
+        });
+    }
+}
